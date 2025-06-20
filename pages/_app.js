a452ce18ea5c5c5271e5aa701545bb9fd6bf5df2
George K <gkurkjian@myseneca.ca>
2025-06-19 20:13:17 -0400

    fifth commit:  Add StaffProfile page to display individual user details.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
 // pages/_app.js
 import '../styles/globals.css'
 import { UserProvider } from '../context/UserContext';
+import Link from 'next/link';
+import Router, { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const handleNavigation = (e) => {
+    e.preventDefault();
+    router.push('/');
+  };
+
   return (
     <UserProvider>
-          <div className="layout">
+      <div className="layout">
       <header>
-        <h1>Fake Staff Directory</h1>
+        <nav>
+          <Link href="/" onClick={handleNavigation}>Home</Link>
+        </nav>
       </header>
 
       <main>
---
+++ pages/staff/[id].js
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { UserContext } from "@/context/UserContext";
+import UserCard from "../components/UserCard";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+export default function StaffProfile() {
+    const { users, error } = useContext(UserContext);
+    const router = useRouter();
+    const { id } = router.query;
+
+    const virtualId = parseInt(id);
+    const user = users[virtualId - 1]; // Adjusting for zero-based index
+
+    if (error) return <p>Error loading user data</p>;
+    if (!users || users.length === 0) return <p>Loading ...</p>;
+    if (!user) return <p>User not found</p>;
+
+    return(
+        <main>
+            <h1>Staff Member</h1>
+            <Link href="/staff">Back to Staff Directory</Link>
+            <UserCard user={user} />
+            <h2>Welcome to {user?.first_name}&apos;s page.</h2><br />
+            <h3>If you would like to keep in touch consider email to {user?.first_name} at <strong>{user?.email}</strong></h3>
+        </main>
+    )
+}
